7197d0aae4ab6fcbe48671a5386787ee104c3698
jjl <james.laver@gmail.com>
2020-12-10 15:09:44 +0000

    don't depend on docker-compose

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 export UID=$(id -u)
 export GID=$(id -g)
-alias darn="docker run -it -u $(id -u):$(id -g) -w /opt/app -p '5000:5000' -v $(pwd):/opt/app -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -v /etc/shadow:/etc/shadow:ro garg-frontend-dev:latest /bin/ls -la"
+alias darn="docker run -it -u $(id -u):$(id -g) -w /opt/app -p '5000:5000' -v $(pwd):/opt/app -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -v /etc/shadow:/etc/shadow:ro garg-frontend-dev:latest yarn"
 alias duild="docker build . -f Dockerfile.dev -t garg-frontend-dev:latest"
